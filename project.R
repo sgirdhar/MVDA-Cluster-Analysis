fd7c81d38526e185f2acd63bf2f97981eff1bc14
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2020-12-30 00:40:37 +0100

    updated project code

--- project.R
+++ project.R
@@ -2,6 +2,8 @@ setwd("D:/MS/SEM 3/MVDA/Exercises/MVDA-Cluster-Analysis")
 
 diet = read.csv("Cluster_Analysis_Survey.csv", header = TRUE, sep = ",")
 
+##################### Data preprocssing  ####################
+
 names(diet)
 # diet = cbind(diet[,2:3], diet[,5:length(diet)])
 
@@ -26,6 +28,8 @@ diet$skip_breakfast = diet$How.many.days.in.a.week.do.you.skip.breakfast.
 diet$alcohol = diet$How.many.days.in.a.week.do.you.consume.alcoholic.beverages.
 diet$main_meal = diet$Most.frequent.source.of.your.main.meal..
 diet$geneder = diet$What.is.your.gender..1
+
+attach(diet)
 
 # delete old columns
 diet = diet[, -c(1:14)]
@@ -55,5 +59,85 @@ diet$geneder = replace(diet$geneder, diet$geneder == 'Female' , '1')
 
 # diet$geneder[is.na(diet$geneder)] = 0
 
+main_meal1 = seq(0, 0, length.out = nrow(diet))
+main_meal2 = seq(0, 0, length.out = nrow(diet))
+
+main_meal1[diet$main_meal == 'Home cooked'] = 1
+main_meal1[diet$main_meal == 'Pre-cooked/microwave'] = 0
+main_meal1[diet$main_meal == 'Canteen'] = 0
+
+main_meal2[diet$main_meal == 'Home cooked'] = 0
+main_meal2[diet$main_meal == 'Pre-cooked/microwave'] = 1
+main_meal2[diet$main_meal == 'Canteen'] = 0
+
+diet$main_meal1 = main_meal1
+diet$main_meal2 = main_meal2
+
+diet = diet[, -1]
+diet = diet[, -12]
+
+# convert meals_per_day '6 or more' to 6
+diet$meals_per_day = as.character(diet$meals_per_day)
+diet$meals_per_day = replace(diet$meals_per_day, diet$meals_per_day == '6 or more', '6')
+
+diet$meals_per_day = as.factor(diet$meals_per_day)
+diet$geneder = as.factor(diet$geneder)
+# typeof(diet$geneder)
+
+################## Cluster Analaysis - Hierarchical ####################
+
+# Ward hierarchical clustering
+
+d = dist(diet, method = "euclidean")
+fit = hclust(d, method = "ward")
+plot(fit)
+groups = cutree(fit, k = 5)
+rect.hclust(fit, k = 5, border = "red")
 
 
+# Avg hierarchical clustering
+
+d1 = dist(diet, method = "euclidean")
+fit1 = hclust(d1, method = "average")
+plot(fit1)
+groups1 = cutree(fit1, k = 5)
+rect.hclust(fit1, k = 5, border = "red")
+
+
+# Complete hierarchical clustering
+
+d2 = dist(diet, method = "euclidean")
+fit2 = hclust(d2, method = "complete")
+plot(fit2)
+groups2 = cutree(fit2, k = 5)
+rect.hclust(fit2, k = 5, border = "red")
+
+
+# Single hierarchical clustering
+
+d3 = dist(diet, method = "euclidean")
+fit3 = hclust(d3, method = "single")
+plot(fit3)
+groups3 = cutree(fit3, k = 5)
+rect.hclust(fit3, k = 5, border = "red")
+
+
+################## Cluster Analaysis - k means ####################
+
+library(cluster)
+
+# K-means clustering with 3 clusters
+fit4 = kmeans(diet, 3)
+fit4
+clusplot(diet, fit4$cluster, color = TRUE, shade = TRUE, labels = 2, lines = 0)
+
+fit5 = kmeans(diet, 2)
+fit5
+clusplot(diet, fit5$cluster, color = TRUE, shade = TRUE, labels = 2, lines = 0)
+
+library(ggplot2)
+library(g)
+
+color =  c("red", "blue", "green", "black")
+clus4 = cutree(diet, 4)
+plot(as.phylo(diet), type = "fan",  tip.color = colors[clus4], label.offset = 1, cex = 0.7)
