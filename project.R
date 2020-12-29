a3cd2a91ee47631278a73fcc7069212319a8a556
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2020-12-29 23:55:29 +0100

    updated project code

--- project.R
+++ project.R
@@ -1,4 +1,4 @@
-setwd("D:/MS/SEM 3/MVDA/Exercises")
+setwd("D:/MS/SEM 3/MVDA/Exercises/MVDA-Cluster-Analysis")
 
 diet = read.csv("Cluster_Analysis_Survey.csv", header = TRUE, sep = ",")
 
@@ -30,5 +30,30 @@ diet$geneder = diet$What.is.your.gender..1
 # delete old columns
 diet = diet[, -c(1:14)]
 
-# impute 'most frequent meal' eggs => home cooked, canteen => restaurant
-d
+# impute 'most frequent meal' Eggs => Home Cooked
+diet$main_meal = as.character(diet$main_meal)
+diet$main_meal = replace(diet$main_meal, diet$main_meal == 'Eggs', 'Home cooked')
+
+nrow(diet)
+region1 = seq(0, 0, length.out = nrow(diet))
+region2 = seq(0, 0, length.out = nrow(diet))
+
+region1[diet$region=='Asia'] = 1
+region1[diet$region=='Europe'] = 0
+region1[diet$region=='Other'] = 0
+
+region2[diet$region=='Asia'] = 0
+region2[diet$region=='Europe'] = 1
+region2[diet$region=='Other'] = 0
+
+diet$region1 = region1
+diet$region2 = region2
+
+diet$geneder = as.character(diet$geneder)
+diet$geneder = replace(diet$geneder, diet$geneder == 'Male', '0')
+diet$geneder = replace(diet$geneder, diet$geneder == 'Female' , '1')
+
+# diet$geneder[is.na(diet$geneder)] = 0
+
+
+
